80f5bbf1a5219aa44360038d151209b255aad49b
reo bamba <12reo08bam@gmail.com>
2020-08-30 09:54:15 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -41,7 +41,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            ]);
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         return redirect('/');
@@ -84,7 +88,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            ]);
         $task = Task::FindOrFail($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         return redirect('/');
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,9 +2,24 @@
 
 @section('content')
 
+@if (count($errors) > 0)
+        <ul class="alert alert-danger" role="alert">
+            @foreach ($errors->all() as $error)
+                <li class="ml-4">{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <h1>TASK新規作成</h1>
     <div class = "row">
         <div class = "col-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+            <div class = "form-group">
+                {!! Form::label('status', 'STATUS') !!}
+                <input type = "text" class = "form-control" name = "status">
+            </div>
+            
             <div class = "form-group">
                 {!! Form::label('content', 'タスク') !!}
                 {!! Form::text('content', null, ['class' => 'form-control']) !!}
